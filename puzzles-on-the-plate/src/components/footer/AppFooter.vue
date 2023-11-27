204adc364771e95302d5a484b5965a0d3167a5b7
Morasiu <morasiu2@gmail.com>
2023-11-27 11:16:26 +0100

    Footer

---
+++ puzzles-on-the-plate/src/components/footer/AppFooter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="footer">
+    <img src="@/assets/logo.png" >
+    <span>© 2023 puzzlenatalerzu.pl</span>
+    <span>Wszystkie prawa zastrzeżone</span>
+    <span>Kopiowanie i rozpowszechnianie bez zgody puzzlenatalerzu.pl zabronione</span>
+    <br>
+    <a>Strona główna</a>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  background-color: color-mix(in srgb, var(--primary) 20%, transparent);
+}
+</style>
