7ffedf924d55c8a637fc1251531ca4833595b111
Morasiu <morasiu2@gmail.com>
2023-12-05 13:17:10 +0100

    Part 1

--- puzzles-on-the-plate/src/service/puzzle/data/recipes.ts
+++ puzzles-on-the-plate/src/service/puzzle/data/recipes.ts
@@ -1,25 +1,29 @@
 import type {Recipe} from "@/service/puzzle/types";
 
-function getImageUrl(name: string) :string {
+function getImageUrl(name: string): string {
     return new URL(`../../../assets/photos/${name}`, import.meta.url).href
 }
 
 export const getRecipesData = (): Recipe[] => {
 
-    const recipes = [
+    const recipes: Recipe[] = [
         {
             name: "Kurczak curry",
             imageUrl: getImageUrl("curry-chicken.jpg"),
-            slug: "curry-chicken"
+            slug: "curry-chicken",
+            shortDescription: "Szybkie danie z piersi z kurczaka doprawione curry",
+            tags: ["kurczak", "curry", "indyjskie", "szybkie", "nowe"]
         }];
     const numberOfSoon = 19;
     for (let i = 0; i < numberOfSoon; i++) {
         recipes.push(
             {
-            name: "Wkrótce",
-            imageUrl: "https://picsum.photos/1000/1000/?random&i=" + i,
-            slug: "soon-" + i
-        })
+                name: "Wkrótce",
+                imageUrl: "https://picsum.photos/1000/1000/?random&i=" + i,
+                slug: "soon-" + i,
+                shortDescription: "soon-desc-" + i,
+                tags:["soon"]
+            })
     }
 
     return recipes;
--- puzzles-on-the-plate/src/views/RecipeView.vue
+++ puzzles-on-the-plate/src/views/RecipeView.vue
@@ -21,8 +21,18 @@ getRecipe(slug).then(x => {
 
 <template>
   <div class="recipe">
-    <div v-if="recipe">
+    <div v-if="recipe" class="header">
+      <img :src="recipe.imageUrl" :alt="recipe.name" class="image">
+      <div class="description">
+        <h1>{{ recipe.name }}</h1>
+        <span>{{ recipe.shortDescription }}</span>
+        <div class="tags">
+          <div v-for="tag in recipe.tags" :key="tag" class="tag">
+            {{ tag }}
+          </div>
+        </div>
 
+      </div>
     </div>
     <div v-else>
       Ładowanie...
@@ -31,12 +41,43 @@ getRecipe(slug).then(x => {
 </template>
 
 <style scoped lang="scss">
-@media (min-width: 1024px) {
-  .recipe {
-    min-height: 100vh;
+.recipe {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  margin: 5rem;
+  @media (max-width: 480px) {
+    margin: 0rem;
+  }
+  .header {
     display: flex;
-    align-items: center;
-    justify-content: center;
+    @media (max-width: 480px) {
+      flex-direction: column;
+    }
+
+    .image {
+      width: 50%;
+      @media (max-width: 480px) {
+        width: 100%;
+      }
+    }
+
+    .description {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      min-width: 50%;
+
+      .tags {
+        display: flex;
+        gap: 1rem;
+        margin-top: 1rem;
+        .tag {
+          background-color: var(--primary);
+          padding: 0rem 0.5rem 0rem 0.5rem;
+        }
+      }
+    }
   }
 }
 </style>
