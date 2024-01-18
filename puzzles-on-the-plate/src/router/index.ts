db554354d3019b541c5dba4f304a1af83df5de05
Morasiu <morasiu2@gmail.com>
2024-01-18 11:48:37 +0100

    Better pipeline

--- puzzles-on-the-plate/src/router/index.ts
+++ puzzles-on-the-plate/src/router/index.ts
@@ -1,9 +1,9 @@
-import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFound from "@/views/NotFound.vue";
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
