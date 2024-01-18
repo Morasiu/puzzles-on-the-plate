27f1d309384acf2a820e7933af7cfd40c5882941
Morasiu <morasiu2@gmail.com>
2024-01-18 11:37:20 +0100

    Alias

--- puzzles-on-the-plate/src/router/index.ts
+++ puzzles-on-the-plate/src/router/index.ts
@@ -13,6 +13,7 @@ const router = createRouter({
         {
             path: '/recipes/:slug',
             name: 'recipes',
+            alias: '/przepisy/:slug',
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
